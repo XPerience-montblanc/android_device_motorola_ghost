0f0332cf13f00ba53f0578308a8b7526dbc4668e
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-09-07 00:49:34 -0500

    Update and rename xpe.mk to cm.mk

--- cm.mk
+++ cm.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
 # Inherit some common XPe stuff.
-$(call inherit-product, vendor/XPe/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/XPe/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := xpe_ghost
+PRODUCT_NAME := cm_ghost
 PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_DEVICE := ghost
 PRODUCT_BRAND := Motorola
